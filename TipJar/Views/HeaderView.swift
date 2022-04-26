4fb668f77cfcdee4fdaf243f4d3ee6d30b367741
Azhar Islam <azhartislam@gmail.com>
2022-04-26 19:45:55 +0100

    Added header view

---
+++ TipJar/Views/HeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  HeaderView.swift
+//  TipJar
+//
+//  Created by Azhar Islam on 26/04/2022.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Image("tipJarLogo")
+                .resizable()
+                .frame(width: 95, height: 30)
+            Spacer()
+            Image("history")
+                .resizable()
+                .frame(width: 25, height: 25)
+                .padding(.trailing, 5)
+        }
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView().previewLayout(.sizeThatFits)
+    }
+}
