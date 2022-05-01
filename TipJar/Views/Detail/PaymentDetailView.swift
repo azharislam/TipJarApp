23c4ab4db9dd3dc18b61ba5a240eefbe3b4fd15d
Azhar Islam <azhartislam@gmail.com>
2022-05-01 07:11:00 +0100

    Added payment detail view

--- TipJar/Views/Detail/PaymentDetailView.swift
+++ TipJar/Views/Detail/PaymentDetailView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct PaymentDetailView: View {
+    
+    var savedPayment: SavedPayment
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            Image(uiImage: (ImageFileManager.fetchImage(savedPayment.image ) ?? UIImage(named: Constants.App.placeholder)) ?? UIImage())
+                .resizable()
+                .scaledToFit()
+                .frame(height: 399)
+                .background(.white)
+                .cornerRadius(12)
+                .overlay(RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 0.3))
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Text(savedPayment.date )
+                    .font(Font.Roboto.medium(size: 16))
+                HStack {
+                    Text(savedPayment.totalAmount )
+                        .font(Font.Roboto.medium(size: 24))
+
+                    Spacer()
+                    Text("Tip: \(savedPayment.tipTotal )")
+                        .font(Font.Roboto.medium(size: 16))
+                        .foregroundColor(Color.tipGray)
+                }
+            }
+            .padding(16)
+            .background(.white)
+            .cornerRadius(12)
+            .overlay(RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.gray, lineWidth: 0.3))
+            .padding(.bottom, 5)
+        }
+        .padding(.horizontal, 38)
     }
 }
 
 struct PaymentDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        PaymentDetailView()
+        PaymentDetailView(savedPayment: SavedPayment(context: DatabaseManager.instance.context))
     }
 }
